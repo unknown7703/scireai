c6a43777d372811db10c9421357fc3c3219d4c42
PranitMalhotra <pranitmalhotra7@gmail.com>
2024-12-20 18:44:56 +0530

    feat: added prompting for chat with groq and context menu lookup

---
+++ src/api/groq.js
@@ -0,0 +1,65 @@
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: "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" });
+
+export async function main() {
+  const prompt = "What is the best way to go about literature reviews?";
+  const chatCompletion = await groqChat(prompt);
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function groqChat(query) {
+  const prompt = `
+  INSTRUCTIONS:
+  1. Do not add ay introductory exclamations or greetings.
+  2. Do not ask any concluding questions.
+  3. Do not provide summaries at the end.
+
+  PROMPT:
+  You are assisting researchers by responding to the QUERY that they have.
+  
+  QUERY: ${query}`;
+
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are an AI assistant that helps researchers.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+export async function contextMenuLookup(text) {
+  const prompt = `
+  INSTRUCTIONS:
+  1. Do not add ay introductory exclamations or greetings.
+  2. Do not ask any concluding questions.
+  3. Do not provide summaries at the end.
+
+  PROMPT:
+  You are assisting researchers by explaining the meaning of TERM that they wish to understand, providing clear explanations along with relevant examples to enhance their comprehension.
+  
+  TERM: ${text}`;
+
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are an AI assistant that helps researchers.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+main();
